0d72a381178206bd2474fd1178b4b0b2a891699b
takimate <127034756+takimate@users.noreply.github.com>
2023-11-05 03:27:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,153 +1,135 @@
 const express = require('express');
-const request = require('request');
-const fs = require('fs');
-const { promisify } = require('util');
 const { Configuration, OpenAIApi } = require('openai');
-const app = express();
+const fs = require('fs').promises;
 
-// Load environment variables with default values
+const app = express();
+const port = process.env.PORT || 3000;
+
+const MAX_LENGTH = 399;
+
+// Environment variables
 const GPT_MODE = process.env.GPT_MODE || 'CHAT';
-const HISTORY_LENGTH = parseInt(process.env.HISTORY_LENGTH) || 5;
+const HISTORY_LENGTH = parseInt(process.env.HISTORY_LENGTH, 10) || 5;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const MODEL_NAME = process.env.MODEL_NAME || 'gpt-3.5-turbo';
 
-if (!OPENAI_API_KEY) {
-    console.log('No OPENAI_API_KEY found. Please set it as an environment variable.');
-    process.exit(1); // Exit the app if API key is missing
+const messages = [{ role: 'system', content: 'You are a helpful Twitch Chatbot.' }];
+let file_context = 'You are a helpful Twitch Chatbot.';
+let last_user_message = '';
+
+async function readContextFile() {
+  try {
+    const data = await fs.readFile('./file_context.txt', 'utf8');
+    console.log('Reading context file and adding it as a system-level message for the agent.');
+    messages[0].content = data;
+  } catch (err) {
+    console.error('Error reading context file:', err);
+  }
 }
 
-// Initialize global variables
-const MAX_LENGTH = 399;
-let fileContext = 'You are a helpful Twitch Chatbot.';
-let lastUserMessage = '';
+async function processChatRequest(text, res, openai) {
+  messages.push({ role: 'user', content: text });
 
-const messages = [{ role: 'system', content: fileContext }];
+  if (messages.length > (HISTORY_LENGTH * 2 + 1)) {
+    messages.splice(1, 2);
+  }
 
-console.log('GPT_MODE is ' + GPT_MODE);
-console.log('History length is ' + HISTORY_LENGTH);
-console.log('OpenAI API Key: ' + OPENAI_API_KEY);
-console.log('Model Name: ' + MODEL_NAME);
+  console.log('Messages:');
+  console.dir(messages);
+  console.log('User Input: ' + text);
+
+  const response = await openai.createChatCompletion({
+    model: MODEL_NAME,
+    messages,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 0.95,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  if (response.data.choices) {
+    let agent_response = response.data.choices[0].message.content;
+
+    console.log('Agent answer: ' + agent_response);
+    messages.push({ role: 'assistant', content: agent_response });
+
+    let sliced_agent_response = agent_response.slice(0, MAX_LENGTH);
+    last_user_message = agent_response.slice(MAX_LENGTH);
+    res.send(sliced_agent_response);
+  } else {
+    res.send('Something went wrong. Try again later!');
+  }
+}
+
+async function processPromptRequest(text, res, openai) {
+  const prompt = `${file_context}\n\nQ:${text}\nA:`;
+
+  console.log('User Input: ' + text);
+
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 0.95,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  if (response.data.choices) {
+    let agent_response = response.data.choices[0].text;
+    console.log('Agent answer: ' + agent_response);
+
+    let sliced_agent_response = agent_response.slice(0, MAX_LENGTH);
+    last_user_message = agent_response.slice(MAX_LENGTH);
+    res.send(sliced_agent_response);
+  } else {
+    res.send('Something went wrong. Try again later!');
+  }
+}
 
 app.use(express.json({ extended: true, limit: '1mb' }));
 
 app.all('/', (req, res) => {
-    console.log('Just got a request!');
-    res.send('Yo!');
+  console.log('Just got a request!');
+  res.send('Yo!');
 });
 
 if (GPT_MODE === 'CHAT') {
-    fs.readFile('./file_context.txt', 'utf8', (err, data) => {
-        if (!err) {
-            console.log('Reading context file and adding it as a system-level message for the agent.');
-            messages[0].content = data;
-        } else {
-            console.error('Error reading file_context.txt:', err);
-        }
-    });
+  readContextFile();
 } else {
-    fs.readFile('./file_context.txt', 'utf8', (err, data) => {
-        if (!err) {
-            console.log('Reading context file and adding it in front of user prompts:');
-            fileContext = data;
-            console.log(fileContext);
-        } else {
-            console.error('Error reading file_context.txt:', err);
-        }
-    });
+  readContextFile().then(() => {
+    console.log('Reading context file and adding it in front of user prompts:');
+    file_context = data;
+    console.log(file_context);
+  });
 }
 
 app.get('/gpt/:text', async (req, res) => {
-    const text = req.params.text;
-    const configuration = new Configuration({
-        apiKey: OPENAI_API_KEY,
-    });
+  const text = req.params.text;
+  const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 
-    const openai = new OpenAIApi(configuration);
-
-    if (GPT_MODE === 'CHAT') {
-        // CHAT MODE EXECUTION
-        messages.push({ role: 'user', content: text);
-
-        if (messages.length > HISTORY_LENGTH * 2 + 1) {
-            console.log('Message amount in history exceeded. Removing oldest user and agent messages.');
-            messages.splice(1, 2);
-        }
-
-        console.log('Conversations in History: ' + (messages.length / 2 - 1) + '/' + HISTORY_LENGTH);
-        console.log('Messages:');
-        console.dir(messages);
-        console.log('User Input: ' + text);
-
-        const response = await openai.createChatCompletion({
-            model: MODEL_NAME,
-            messages: messages,
-            temperature: 0.7,
-            max_tokens: 256,
-            top_p: 0.95,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-        });
-
-        if (response.data.choices) {
-            let agentResponse = response.data.choices[0].message.content;
-            messages.push({ role: 'assistant', content: agentResponse);
-
-            let slicedAgentResponse = agentResponse.slice(0, MAX_LENGTH);
-
-            if (agentResponse.length > MAX_LENGTH) {
-                console.log('Agent answer exceeds Twitch chat limit. Slicing to first 399 characters.');
-                lastUserMessage = agentResponse.slice(MAX_LENGTH);
-                console.log('Sliced Agent answer: ' + agentResponse);
-            }
-            res.send(slicedAgentResponse);
-        } else {
-            res.send('Something went wrong. Try again later!');
-        }
-    } else {
-        // PROMPT MODE EXECUTION
-        const prompt = `${fileContext}\n\nQ:${text}\nA:`;
-        console.log('User Input: ' + text);
-
-        const response = await openai.createCompletion({
-            model: 'text-davinci-003',
-            prompt: prompt,
-            temperature: 0.7,
-            max_tokens: 256,
-            top_p: 0.95,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-        });
-
-        if (response.data.choices) {
-            let agentResponse = response.data.choices[0].text;
-
-            let slicedAgentResponse = agentResponse.slice(0, MAX_LENGTH);
-
-            if (agentResponse.length > MAX_LENGTH) {
-                console.log('Agent answer exceeds Twitch chat limit. Slicing to first 399 characters.');
-                lastUserMessage = agentResponse.slice(MAX_LENGTH);
-                console.log('Sliced Agent answer: ' + agentResponse);
-            }
-            res.send(slicedAgentResponse);
-        } else {
-            res.send('Something went wrong. Try again later!');
-        }
-    }
+  if (GPT_MODE === 'CHAT') {
+    processChatRequest(text, res, openai);
+  } else {
+    processPromptRequest(text, res, openai);
+  }
 });
 
 app.all('/continue/', (req, res) => {
-    console.log(lastUserMessage);
-    console.log('Just got a continue request!');
-
-    if (lastUserMessage.length > 0) {
-        let newuserMessage = lastUserMessage.slice(0, MAX_LENGTH);
-        lastUserMessage = lastUserMessage.slice(MAX_LENGTH);
-
-        console.log('Sliced Agent answer: ' + lastUserMessage);
-        res.send(newuserMessage);
-    } else {
-        res.send('No message to continue. Please send a new message first.');
-    }
+  if (last_user_message.length > 0) {
+    let new_user_message = last_user_message.slice(0, MAX_LENGTH);
+    last_user_message = last_user_message.slice(MAX_LENGTH);
+    res.send(new_user_message);
+  } else {
+    res.send('No message to continue. Please send a new message first.');
+  }
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
